c7834f1642b1c7b49c11702c1db1584cf6d44394
Erik Svensson <eosvensson@gmail.com>
2021-01-19 08:39:25 +0100

    Reset

--- src/main/java/se/lexicon/functional_lesson_three/App.java
+++ src/main/java/se/lexicon/functional_lesson_three/App.java
@@ -1,15 +1,17 @@
 package se.lexicon.functional_lesson_three;
 
-import java.util.List;
-
 import se.lexicon.functional_lesson_three.model.Person;
 import se.lexicon.functional_lesson_three.util.PersonGenerator;
 
+import java.util.List;
+
 public class App {
 	
-    public static void main( String[] args ){
-        List<Person> persons = PersonGenerator.getInstance().generate(1000);
-        
+    public static void main( String[] args ) {
+        List<Person> persons = PersonGenerator.getInstance().generate(10000);
     }
+
+
+
     
 }
